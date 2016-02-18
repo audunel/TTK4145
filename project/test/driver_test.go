7e6282ec0dedeb44fbba895bc3251b5516dddbb4
theAssBlaster <blasting@ass.co.uk>
2016-02-18 14:27:38 +0100

    Added project folder to seperate exercises from project. Also started using go unit testing framework

---
+++ project/test/driver_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"../driver"
+	"time"
+	"testing"
+)
+
+const deadline = 30
+
+func TestDriver(t *testing.T) {
+	driver.ElevInit()
+
+	driver.SetMotorDirection(driver.DirnUp)
+	go func() {
+		time.Sleep(deadline * time.Second)
+		driver.SetMotorDirection(driver.DirnStop)
+		t.Fatalf("Elevator test failed to finish in %d seconds", deadline)
+	}()
+	for driver.GetFloorSignal() != driver.NumFloors - 1 {}
+	driver.SetMotorDirection(driver.DirnDown)
+	for driver.GetFloorSignal() != 0 {}
+	driver.SetMotorDirection(driver.DirnStop)
+}
