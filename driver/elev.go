365b81d3148c64d10ec88c6aa22d159cf3a4c434
theAssBlaster <blasting@ass.co.uk>
2016-02-18 12:36:32 +0100

    Small fixes

--- driver/elev.go
+++ driver/elev.go
@@ -25,19 +25,13 @@ const (
 
 func ElevInit() { C.elev_init() }
 
-func SetMotorDirection(dirn MotorDirection) {
-	C.elev_set_motor_direction(C.elev_motor_direction_t(dirn))
-}
-func SetButtonLamp(button ButtonType, floor, value int) {
-	C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor), C.int(value))
-}
+func SetMotorDirection(dirn MotorDirection) { C.elev_set_motor_direction(C.elev_motor_direction_t(dirn)) }
+func SetButtonLamp(button ButtonType, floor, value int) { C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor), C.int(value)) }
 func SetFloorIndicator(floor int) { C.elev_set_floor_indicator(C.int(floor)) }
 func SetDoorOpenLamp(value int)   { C.elev_set_door_open_lamp(C.int(value)) }
 func SetStopLamp(value int)       { C.elev_set_stop_lamp(C.int(value)) }
 
-func GetButtonSignal(button ButtonType, floor int) int {
-	return int(C.elev_get_button_signal(C.elev_button_type_t(button), C.int(floor)))
-}
+func GetButtonSignal(button ButtonType, floor int) int { return int(C.elev_get_button_signal(C.elev_button_type_t(button), C.int(floor))) }
 func GetFloorSignal() int       { return int(C.elev_get_floor_sensor_signal()) }
 func GetStopSignal() int        { return int(C.elev_get_stop_signal()) }
 func GetObstructionSignal() int { return int(C.elev_get_obstruction_signal()) }
