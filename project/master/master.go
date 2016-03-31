f4f041475d0272a5b9bdbb09c3b0c8c1238fae66
Mads <YOUR EMAIL ADDRESS>
2016-03-31 19:05:34 +0200

    Further work on master module

--- project/master/master.go
+++ project/master/master.go
@@ -8,20 +8,62 @@ import (
 	"fmt"
 )
 
-const clientTimeout = 5 * time.Second
+const slaveTimeoutPeriod = 5 * time.Second
+const selfAsBackupDeadline = 10 * time.Second
+var myID = network.GetOwnID()
 
-func WaitForBackup(events com.MasterEvent, initialQueue []com.Order,
-					initialSlaves map[network.ID]com.Slave) {
+func InitMaster(events com.MasterEvent,
+		initialQueue []com.Order,
+		initialSlaves map[network.ID]com.Slave) {
 
-	myID := network.GetOwnID()
-	fmt.Printf("Waiting for backup on machine %s\n", myID.String())
+	selfAsBackup := false
 
 	queue	:= initialQueue
 	slaves	:= initialSlaves
 
 	for {
-		packet := <- events.FromSlave
-		_, err := com.DecodeSlaveData
-		if err != nil {
-			break
+		fmt.Printf("Waiting for backup on machine %s\n", myID)
+		select {
+		case <- time.After(selfAsBackupDeadline):
+			selfAsBackup = true
+
+		case packet := <- events.FromSlave:
+			_, err := com.DecodeSlaveData(packet.Data)
+			if err != nil {
+				break
+			}
+
+			if(packet.Address == myID && selfAsBackup) || (packet.Address != myID) {
+				if packet.Address == myID {
+					fmt.Println("Using self as backup")
+				}
+				queue, slaves = masterLoop(events, packet.Address, queue, slaves)
+			}
 		}
+	}
+}
+
+func masterLoop(events com.MasterEvent,
+		backup network.ID,
+		initialQueue []com.Order,
+		initialSlaves map[network.ID]com.Slave) ([]com.Order, map[network.ID]com.Slave) {
+
+	slaveTimedOut := make(chan network.ID)
+
+	if initialQueue == nil {
+		orders := make([]com.Order, 0)
+	} else {
+		orders := initialQueue
+	}
+
+	slaves := make(map[network.ID]com.Slave)
+	if initialSlaves != nil {
+		for _, s := range(intialSlaves) {
+			s.AliveTimer = time.NewTimer(slaveTimeoutPeriod)
+			slaves[s.ID] = s
+			go listenForTimeout(s, slaveTimedOut)
+		}
+	}
+
+	// ADD LOOP HERE
+}
