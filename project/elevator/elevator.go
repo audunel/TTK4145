6b73bf2091860879090169a840dbed7dfffd7ccc
Student <student@itk-d650>
2016-04-05 12:34:28 +0200

    Refactored elevator module to conform to naming conventions

--- project/elevator/elevator.go
+++ project/elevator/elevator.go
@@ -1,118 +1,122 @@
 package elevator
 
 import (
-    "time"
 	"../driver"
-    "../logger"
+	"time"
 )
 
+const deadlinePeriod	= 5 * driver.NumFloors * time.Second
+const doorPeriod		= 3 * time.Second
+var lastPassedFloor int
 
-const deadline_period = 20 * time.Second
-const door_period = 3 * time.Second
-var last_passed_floor int
-
-type state_t int
-const (
-    idle state_t = iota
-    doorOpen
-    moving
-)
+type state int
+const {
+	idle state = iota
+	doorOpen
+	moving
+}
 
 func GetLastPassedFloor() int {
-    return last_passed_floor
+	return lastPassedFloor
 }
 
 func Init(
-    completed_floor  chan <- int,
-    missed_deadline  chan <- bool,
-    floor_reached    <- chan int,
-    new_target_floor <- chan int,
-    stop_button      <- chan bool) {
+		completedFloor	chan <- int,
+		missedDeadline	chan <- bool,
+		floorReached	<- chan int,
+		newTargetFloor	<- chan int,
+		stopButton		<- chan bool,
+		obstruction		<- chan bool) {
 
+	deadlineTimer := time.NewTimer(deadlinePeriod)
+	deadlineTimer.stop()
 
-    deadline_timer := time.NewTimer(deadline_period)
-    deadline_timer.Stop()
+	doorTimer := time.NewTimer(doorPeriod)
+	doorTimer.Stop()
 
-    door_timer := time.NewTimer(door_period)
-    door_timer.Stop()
+	state := idle
+	lastPassedFloor = 0
+	targetFloor = -1
 
-    state := idle
-    last_passed_floor = 0
-    target_floor := -1
+	for {
+		select {
+		case <- deadlineTimer.C:
+			missedDeadline <- true
 
-    for {
-        select {
-        case <- door_timer.C:
-            switch (state) {
-                case doorOpen:
-                    //println(logger.Info, "Door timer @ doorOpen")
-                    driver.SetDoorOpenLamp(0)
-                    state = idle
-                    //completed_floor <- target_floor
-                    target_floor = -1
-                    deadline_timer.Stop()
-                case idle:    //println(logger.Debug, "Door timer @ idle")
-                case moving:  //println(logger.Debug, "Door timer @ moving")
-            }
+		case <- doorTimer.C:
+			switch state {
 
-        case <- deadline_timer.C:
-            missed_deadline <- true
+				case doorOpen:
+					fmt.Println("Door timer, state at doorOpen")
+					driver.SetDoorOpenLamp(0)
+					state = idle
+					completedFloor <- targetFloor
+					targetFloor = -1
+					deadlineTimer.Stop()
+				case idle:
+					fmt.Println("Door timer, state at idle")
+				case moving:
+					fmt.Println("Door timer, state at moving")
+			}
 
-        case floor := <- new_target_floor:
-            if target_floor != floor {
-                deadline_timer.Reset(deadline_period)
-            }
-            target_floor = floor
-            switch (state) {
-                case idle:
-                    //println(logger.Info, "New order @ idle")
-                    if target_floor == -1 {
-                        break
-                    } else if target_floor > last_passed_floor {
-                        state = moving
-                        driver.SetMotorDirection(driver.DirnUp)
-                    } else if target_floor < last_passed_floor {
-                        state = moving
-                        driver.SetMotorDirection(driver.DirnDown)
-                    } else {
-                        door_timer.Reset(door_period)
-                        driver.SetDoorOpenLamp(1)
-                        driver.SetMotorDirection(driver.DirnStop)
-                        state = doorOpen
-                    }
-                case moving:  // println(logger.Debug, "New order @ moving")
-                case doorOpen: //println(logger.Debug, "New order @ doorOpen")
-            }
+		case floor := <- newTargetFloor:
+			if targetFloor != floor {
+				deadlineTimer.Reset(deadlinePeriod)
+			}
+			targetFloor = floor
+			switch state {
+				case idle:
+					fmt.Println("New order, state at idle")
+					if targetFloor == -1 {
+						break
+					} else if targetFloor > lastPassedFloor {
+						state = moving
+						driver.SetMotorDirection(driver.DirnUp)
+					} else if targetFloor < lastPassedFloor {
+						state = moving
+						driver.SetMotorDirection(driver.DirnDown)
+					} else {
+						doorTimer.Reset(doorPeriod)
+						driver.SetDoorOpenLamp(1)
+						driver.SetMotorDirection(driver.DirnStop)
+						state = doorOpen
+					}
+				case moving:
+					fmt.Println("New order, state at moving")
+				case doorOpen:
+					fmt.Println("New order, state at doorOpen")
+			}
 
-        case floor := <- floor_reached:
-            last_passed_floor = floor
-            switch (state) {
-                case moving:
-                    //println(logger.Info, "Reached floor", floor, "@ moving")
-                    driver.SetFloorIndicator(floor)
-                    if target_floor == -1 {
-                        break
-                    } else if target_floor > last_passed_floor {
-                        state = moving
-                        driver.SetMotorDirection(driver.DirnUp)
-                    } else if target_floor < last_passed_floor {
-                        state = moving
-                        driver.SetMotorDirection(driver.DirnDown)
-                    } else {
-                        door_timer.Reset(door_period)
-                        driver.SetDoorOpenLamp(1)
-                        driver.SetMotorDirection(driver.DirnStop)
-                        state = doorOpen
-                    }
-                case idle:    // println(logger.Info, "Reached floor", floor, "@ idle")
-                case doorOpen:// println(logger.Info, "Reached floor", floor, "@ doorOpen")
-            }
+		case floor := <- floorReached:
+			lastPassedFloor = floor
+			switch state {
+				case moving:
+					fmt.Printf("Reached floor %d, state at moving\n", floor)
+					driver.SetFloorIndicator(floor)
+					if targetFloor == -1 {
+						break
+					} else if targetFloor > lastPassedFloor {
+						state = moving
+						driver.SetMotorDirection(driver.DirnUp)
+					} else if targetFloor < lastPassedFloor {
+						state = moving
+						driver.SetMotorDirection(driver.DirnDown)
+					} else {
+						doorTimer.Reset(doorPeriod)
+						driver.SetDoorOpenLamp(1)
+						driver.SetMotorDirection(driver.DirnStop)
+					}
+				case idle:
+					fmt.Printf("Reached floor %d, state at idle\n", floor)
+				case doorOpen:
+					fmt.Println("Reached floor %d, state at doorOpen\n", floor)
+			}
 
-        case <- stop_button:// Ignoring
-        }
-    }
-}
+		case <- stopButton:
+			// Functionality not implemented
 
-func println(level logger.Level, args...interface{}) {
-    logger.Println(level, "LIFT", args...)
+		case <- obstruction:
+			// Functionality not implemented
+		}
+	}
 }
