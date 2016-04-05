990c68388c249af99d702f4815b1c55c168c6561
Audun Eltvik <eltvik94@gmail.com>
2016-04-05 13:06:49 +0200

    Minor change to driver test

--- project/test/driver_test.go
+++ project/test/driver_test.go
@@ -4,19 +4,23 @@ import (
 	"../driver"
 	"time"
 	"testing"
+	"fmt"
 )
 
-const deadline = 20
+const deadline = time.Duration(5 * driver.NumFloors) * time.Second
 
 func TestDriver(t *testing.T) {
 	driver.ElevInit()
 
 	driver.SetMotorDirection(driver.DirnUp)
+	
 	go func() {
-		time.Sleep(deadline * time.Second)
+		time.Sleep(deadline)
 		driver.SetMotorDirection(driver.DirnStop)
 		t.Fatalf("Elevator test failed to finish in %d seconds", deadline)
 	}()
+
+	fmt.Printf("%d\n", driver.GetFloorSignal())
 	for driver.GetFloorSignal() != driver.NumFloors - 1 {}
 	driver.SetMotorDirection(driver.DirnDown)
 	for driver.GetFloorSignal() != 0 {}
