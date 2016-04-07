653e1dd47217a04728c418706978565ddc8a36a9
Audun Eltvik <eltvik94@gmail.com>
2016-04-07 14:03:53 +0200

    Driver test now works as intended

--- project/test/driver_test.go
+++ project/test/driver_test.go
@@ -7,22 +7,35 @@ import (
 	"fmt"
 )
 
-const deadline = time.Duration(5 * driver.NumFloors) * time.Second
+const deadline = 5 * driver.NumFloors
 
 func TestDriver(t *testing.T) {
 	driver.ElevInit()
 
 	driver.SetMotorDirection(driver.DirnUp)
-	
+
+	timeout := make(chan bool)
+	success := make(chan bool)
+
 	go func() {
-		time.Sleep(deadline)
+		time.Sleep(time.Duration(deadline) * time.Second)
 		driver.SetMotorDirection(driver.DirnStop)
-		t.Fatalf("Elevator test failed to finish in %d seconds", deadline)
+		timeout <- true
 	}()
 
-	fmt.Printf("%d\n", driver.GetFloorSignal())
-	for driver.GetFloorSignal() != driver.NumFloors - 1 {}
-	driver.SetMotorDirection(driver.DirnDown)
-	for driver.GetFloorSignal() != 0 {}
-	driver.SetMotorDirection(driver.DirnStop)
+	go func() {
+		fmt.Printf("%d\n", driver.GetFloorSignal())
+		for driver.GetFloorSignal() != driver.NumFloors - 1 {}
+		driver.SetMotorDirection(driver.DirnDown)
+		for driver.GetFloorSignal() != 0 {}
+		driver.SetMotorDirection(driver.DirnStop)
+		success <- true
+	}()
+
+	select {
+	case <- timeout:
+		t.Fatalf("Elevator test failed to finish within %d seconds", deadline)
+	case <- success:
+		t.Logf("Elevator test succeeded within %d seconds", deadline)
+	}
 }
