c01a3f1116e46740ff722fab0a68305daa84cab9
gitariansen <elsa.ariansen@gmail.com>
2017-02-24 12:15:01 +0100

    Removed useless line

--- project/main.go
+++ project/main.go
@@ -17,8 +17,6 @@ import (
 func main() {
 	mainLogger := logger.NewLogger("MAIN")
 
-	fmt.Println("Hello world!")
-
 	var startAsMaster, recoverBackup bool
 	flag.BoolVar(&startAsMaster, "master", false, "Start as master")
 	flag.BoolVar(&recoverBackup, "recover", false, "Recover backup data from disk (Must be master!)")
